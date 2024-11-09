f13295d3ddf6281300f6b4c9984dc4973eb3391e
GOrtiz2305 <160190916+GOrtiz2305@users.noreply.github.com>
2024-11-09 00:38:46 -0600

    Limpieza inicial

---
+++ main/src/layouts/full/vertical/sidebar/MenuItems.js
@@ -0,0 +1,75 @@
+import {
+  IconPoint,
+  IconAperture,
+  IconAppWindow,
+} from '@tabler/icons';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+  {
+    navlabel: true,
+    subheader: 'Home',
+  },
+
+  {
+    id: uniqueId(),
+    title: 'Modern',
+    icon: IconAperture,
+    href: '/dashboards/modern',
+    chip: 'New',
+    chipColor: 'secondary',
+  },
+  {
+    id: uniqueId(),
+    title: 'Frontend pages',
+    icon: IconAppWindow,
+    href: '/frontend-pages/',
+    children: [
+      {
+        id: uniqueId(),
+        title: 'Homepage',
+        icon: IconPoint,
+        href: '/frontend-pages/homepage',
+      },
+      {
+        id: uniqueId(),
+        title: 'About Us',
+        icon: IconPoint,
+        href: '/frontend-pages/about',
+      },
+      {
+        id: uniqueId(),
+        title: 'Blog',
+        icon: IconPoint,
+        href: '/frontend-pages/blog',
+      },
+      {
+        id: uniqueId(),
+        title: 'Blog Details',
+        icon: IconPoint,
+        href: '/frontend-pages/blog/Blog_1',
+      },
+      {
+        id: uniqueId(),
+        title: 'Contact',
+        icon: IconPoint,
+        href: '/frontend-pages/contact',
+      },
+      {
+        id: uniqueId(),
+        title: 'Portfolio',
+        icon: IconPoint,
+        href: '/frontend-pages/portfolio',
+      },
+      {
+        id: uniqueId(),
+        title: 'Pricing',
+        icon: IconPoint,
+        href: '/frontend-pages/pricing',
+      },
+    ],
+  },
+];
+
+export default Menuitems;
