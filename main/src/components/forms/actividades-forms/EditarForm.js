047356c44647ec0559f2c82f40ca75d791138384
GOrtiz2305 <160190916+GOrtiz2305@users.noreply.github.com>
2025-02-11 17:07:19 -0600

    Colaboradores areglados en actividad

--- main/src/components/forms/actividades-forms/EditarForm.js
+++ main/src/components/forms/actividades-forms/EditarForm.js
@@ -19,8 +19,36 @@ import CustomSelect from '../theme-elements/CustomSelect';
 
 const ActividadesEditarForm = ({ id }) => {
   const [proyectos, setProyectos] = useState([]);
-  const [direcciones, setDirecciones] = useState([]);
+  const [direccion, setDireccion] = useState([
+    { id: 0, 
+      detalle: "", 
+      municipio: { 
+        nombre: "",
+        departamento : {
+          nombre: ""
+        }
+      } 
+    }
+  ]);
+  const [departamentos, setDepartamentos] = useState([]);
+  const [municipios, setMunicipios] = useState([]);
+  const [actividadColaboradores, setActividadColaboradores] = useState([
+    {
+      id: 0,
+      id_actividad: 0,
+      id_usuario: 0,
+      usuario: {
+        id: 0,
+        persona: {
+          nombre: "",
+          apellido: "",
+        },
+      },
+    },
+  ]);
+  const [colaboradores, setColaboradores] = useState([]);
   const navigate = useNavigate();
+
   const [data, setData] = useState({
     id: 0,
     fecha_inicio: "",
@@ -28,6 +56,7 @@ const ActividadesEditarForm = ({ id }) => {
     descripcion: "",
     id_proyectos: "",
     id_direccion: "",
+    id_encargado: "",
   });
   const [loading, setLoading] = useState(true);
 
@@ -57,14 +86,42 @@ const ActividadesEditarForm = ({ id }) => {
       }
     };
 
-    const fetchDirecciones = async () => {
+    const fetchColaboradores = async () => {
       try {
-        const response = await fetch(`${URL}direcciones`);
+        const response = await fetch(`${URL}usuarios`);
         if (response.ok) {
           const data = await response.json();
-          setDirecciones(data);
+          setColaboradores(data);
         } else {
-          console.error('Error al obtener las direcciones');
+          console.error('Error al obtener los colaboradores');
+        }
+      } catch (error) {
+        console.error('Error al llamar a la API:', error);
+      }
+    };
+
+    const fetchActividadColaboradores = async () => {
+      try {
+        const response = await fetch(`${URL}actividadColaboradores/actividad/${id}`);
+        if (response.ok) {
+          const data = await response.json();
+          setActividadColaboradores(data);
+        } else {
+          console.error('Error al obtener los colaboradores de la actividad');
+        }
+      } catch (error) {
+        console.error('Error al llamar a la API:', error);
+      }
+    };
+
+    const fetchDepartamentos = async () => {
+      try {
+        const response = await fetch(`${URL}departamentos`);
+        if (response.ok) {
+          const data = await response.json();
+          setDepartamentos(data);
+        } else {
+          console.error('Error al obtener los departamentos');
         }
       } catch (error) {
         console.error('Error al llamar a la API:', error);
@@ -73,15 +130,57 @@ const ActividadesEditarForm = ({ id }) => {
 
     if (id) {
       fetchActividades();
+      fetchActividadColaboradores();
     }
 
     fetchProyectos();
-    fetchDirecciones();
+    fetchDepartamentos();
+    fetchColaboradores();
   }, [id]);
+
+  useEffect(() => {
+    const fetchDireccion = async () => {
+      try {
+        const response = await fetch(`${URL}direcciones/${data.id_direccion}`);
+        if (response.ok) {
+          const data = await response.json();
+          setDireccion(data);
+        } else {
+          console.error('Error al obtener la dirección');
+        }
+      } catch (error) {
+        console.error('Error al llamar a la API:', error);
+      }
+    };
+
+    fetchDireccion();
+  }, [data.id_direccion]);
+
+  const obtenerMunicipios = async (id_departamento) => {
+    try {
+      const response = await fetch(`${URL}municipios/departamento/${id_departamento}`);
+      if (response.ok) {
+        const data = await response.json();
+        setMunicipios(data);
+      } else {
+        console.error('Error al obtener los municipios');
+      }
+    } catch (error) {
+      console.error('Error al llamar a la API:', error);
+    }
+  };
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setData({ ...data, [id]: value });
+  };
+
+  const handleSelectChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const handleUpdate = async (e) => {
@@ -99,17 +198,32 @@ const ActividadesEditarForm = ({ id }) => {
             nombre: data.nombre,
             descripcion: data.descripcion,
             id_proyectos: data.id_proyectos,
-            id_direccion: data.id_direccion,
+            id_encargado: data.id_encargado,
           }
         ),
       });
 
-      if (response.ok) {
+      // Actualizar colaboradores
+      const colaboradoresPorEnviar = (actividadColaboradores || []).map((colaborador) => ({
+        id_colaborador: Number(colaborador.id_colaborador),
+        id_actividad: Number(data.id),
+      }));
+
+      const responseColaboradores = await fetch(`${URL}actividadColaboradores/actividad/${data.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(colaboradoresPorEnviar),
+      });
+
+      if (response.ok && responseColaboradores.ok) {
         alert('Actividad actualizada con éxito');
         navigate('/actividades');
       } else {
         alert('Error al actualizar la actividad');
       }
+
     } catch (error) {
       console.error('Error al llamar a la API:', error);
       alert('Error al llamar a la API');
@@ -154,26 +268,21 @@ const ActividadesEditarForm = ({ id }) => {
         </Grid>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-            {/* <CustomFormLabel htmlFor="id_encargado">Encargado</CustomFormLabel>
+            <CustomFormLabel htmlFor="id_encargado">Encargado</CustomFormLabel>
             <CustomSelect
               id="id_encargado"
               name="id_encargado"
-              value={formik.values.id_encargado}
-              onChange={formik.handleChange}
+              value={data.id_encargado}
+              onChange={handleSelectChange}
               fullWidth
               variant="outlined"
             >
-              {usuarios.map((usuario) => (
+              {colaboradores.map((usuario) => (
                 <MenuItem key={usuario.id} value={usuario.id}>
                   {usuario.persona.nombre} {usuario.persona.apellido}
                 </MenuItem>
               ))}
             </CustomSelect>
-            {formik.errors.id_encargado && (
-              <FormHelperText error>
-                {formik.errors.id_encargado}
-              </FormHelperText>
-            )} */}
           </Grid>
           <Grid item xs={12} sm={6}>
             <CustomFormLabel htmlFor="colaboradores">Colaboradores</CustomFormLabel>
@@ -184,36 +293,35 @@ const ActividadesEditarForm = ({ id }) => {
                 id="colaboradores"
                 name="colaboradores"
                 multiple
-                value={anticipoGastos.rubros || []}
-                onChange={(e) =>
-                  setAnticipoGastos({
-                    ...anticipoGastos,
-                    rubros: e.target.value, // Actualiza el estado con los rubros seleccionados
-                  })
-                }
-                input={<OutlinedInput id="select-multiple-chip" label="Seleccione los rubros" />}
+                value={actividadColaboradores.map(ac => ac.id_colaborador)}
+                onChange={(e) => {
+                  const selectedIds = e.target.value;
+                  setActividadColaboradores(
+                    selectedIds.map(id => ({
+                      id_colaborador: id,
+                      usuario: colaboradores.find(c => c.id === id) || { persona: { nombre: "", apellido: "" } }
+                    }))
+                  );
+                }}
+                input={<OutlinedInput id="select-multiple-chip" label="Seleccione los cooperantes" />}
                 renderValue={(selected) => (
                   <div style={{ display: 'flex', flexWrap: 'wrap', gap: '8px' }}>
-                    {selected.map((rubroId) => {
-                      const rubro = rubrosOptions.find((r) => r.id === rubroId);
-                      return <Chip key={rubroId} label={rubro ? rubro.nombre_rubro : ''} />;
+                    {selected.map(id => {
+                      const colaborador = colaboradores.find(c => c.id === id);
+                      return (
+                        <Chip key={id} label={colaborador ? `${colaborador.persona.nombre} ${colaborador.persona.apellido}` : 'Desconocido'} />
+                      );
                     })}
                   </div>
                 )}
               >
-                {rubrosOptions.map((rubro) => (
-                  <MenuItem key={rubro.id} value={rubro.id}>
-                    {rubro.nombre_rubro}
+                {colaboradores.map((colaborador) => (
+                  <MenuItem key={colaborador.id} value={colaborador.id}>
+                    {colaborador.persona.nombre} {colaborador.persona.apellido}
                   </MenuItem>
                 ))}
               </Select>
             </FormControl>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-          </Grid>
-          <Grid item xs={12} sm={6}>
           </Grid>
         </Grid>
         <CustomFormLabel htmlFor="nombre">Nombre de la Actividad</CustomFormLabel>
@@ -236,19 +344,14 @@ const ActividadesEditarForm = ({ id }) => {
         />
 
         <CustomFormLabel htmlFor="id_direccion">Dirección</CustomFormLabel>
-        <Select
+        <CustomTextField
           id="id_direccion"
-          fullWidth
           variant="outlined"
-          value={data.id_direccion}
-          onChange={(e) => setData({ ...data, id_direccion: e.target.value })}
-        >
-          {direcciones.map((direccion) => (
-            <MenuItem key={direccion.id} value={direccion.id}>
-              {direccion.detalle}
-            </MenuItem>
-          ))}
-        </Select>
+          value={`${direccion?.detalle || ''}, ${direccion?.municipio?.nombre || ''}, ${direccion?.municipio?.departamento?.nombre || ''}`}
+          fullWidth
+          disabled
+        />
+
         <br /> <br />
         <Button color="primary" variant="contained" type="submit">
           Guardar
